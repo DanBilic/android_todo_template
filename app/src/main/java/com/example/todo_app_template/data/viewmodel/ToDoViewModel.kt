2798cdf03a04b869c4e24ad219611c0f0356d90c
DanBilic <bilic90@gmx.de>
2021-07-08 11:14:23 +0200

    added ToDoViewModel.kt

---
+++ app/src/main/java/com/example/todo_app_template/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.todo_app_template.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todo_app_template.data.ToDoDatabase
+import com.example.todo_app_template.data.models.ToDoData
+import com.example.todo_app_template.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application):AndroidViewModel(application) {
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository
+
+    private val getALlData : LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getALlData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData){
+        // start coroutines in view model scope
+        // view model scope is part of kotlin coroutines
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.inserData(toDoData)
+        }
+    }
+}
